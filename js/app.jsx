e924b05a6645e19f821ae06971b8fecb5e0c8ff7
sergeiavdeev <sergei_avdeev@mail.ru>
2017-04-02 13:16:38 +0400

    Add files via upload

--- js/app.jsx
+++ js/app.jsx
@@ -1,1 +1,65 @@
+class App extends React.Component{
+    
+    constructor(props){
+        super(props);                                 
+        
+        var deck = [];        
+        var suit = 0;
+        var val = 2;
+        for(var i = 0; i < 52; i++){
+            
+            deck[i] = {suit: suit, val: val, img: "k" + (i + 1) + ".png", state: false};
+            
+            suit ++;
+                        
+            if(suit > 3){
+                suit = 0;
+                val ++;
+            }
+        }
+        
+        var newDeck = [];
+        
+        for(var i = 50; i >= 0; i--){
+            
+            var num = Math.floor(Math.random() * i);
+                                    
+            newDeck[50 - i] = deck[num];
+            deck.splice(num, 1);
+        }
+        
+        newDeck[51] = deck[0];
+        
+        this.state = {
+                deck: newDeck
+        };
+    }    
+    
+    render(){  
+        
+        var comp = this;
+    
+        return (
+        <div>
+            {this.state.deck.map(function(el, key){
+                return <img className = "card" key={key} src={el.state==true?"img/"+ el.img:"img/k0.png"} onClick={(e)=>comp.cardClick(e, key)} />;
+                })
+            }            
+        </div>
+        );
+    }
 
+    cardClick(e, key){
+        
+        var deck = this.state.deck.slice();
+        
+        deck[key].state = !deck[key].state;
+        
+        this.setState({deck: deck});
+    }
+}
+
+ReactDOM.render(
+        <App />,
+        document.getElementById("app")
+    );
